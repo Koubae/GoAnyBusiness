72ea472d9293746995fe3416b8710c263e96cd1f
koubae <58447627+Koubae@users.noreply.github.com>
2025-09-23 14:02:33 +0200

    improvements

--- internal/app/server.go
+++ internal/app/server.go
@@ -37,7 +37,7 @@ func Run() {
 	)
 	err := router.SetTrustedProxies(config.TrustedProxies)
 	if err != nil {
-		panic(err.Error())
+		log.Fatalf("Error setting trusted proxies, error: %s", err.Error())
 	}
 
 	index := router.Group("/")
@@ -73,76 +73,72 @@ func Run() {
 	}
 
 	srvName := fmt.Sprintf("Service %s-V%s (%s)", config.AppName, config.AppVersion, config.GetAddr())
+	handler := http.MaxBytesHandler(router, 8<<20) // server: cap request body size (e.g., 8 MiB)
 	srv := &http.Server{
 		Addr:              config.GetAddr(),
-		Handler:           router,
+		Handler:           handler,
 		ReadHeaderTimeout: 5 * time.Second,
 		ReadTimeout:       15 * time.Second,
 		WriteTimeout:      30 * time.Second,
 		IdleTimeout:       120 * time.Second,
 	}
 
-	shutdownErr := make(chan error, 1)
+	startUpErr := make(chan error, 1)
 	go func() {
-		log.Printf("%s | Server starting...\n", srvName)
+		log.Printf("%s | Server starting...", srvName)
 		if err := srv.ListenAndServe(); err != nil && !errors.Is(err, http.ErrServerClosed) {
-			shutdownErr <- fmt.Errorf("%s - error while shutting down server, error: %v", srvName, err)
+			startUpErr <- fmt.Errorf("server issues while listening: %v", err)
 			return
 		}
-		shutdownErr <- nil
+		startUpErr <- nil
 	}()
-	log.Printf("%s | Server started\n", srvName)
+	log.Printf("%s | Server started", srvName)
 
 	sigCh := make(chan os.Signal, 1)
 	signal.Notify(sigCh, syscall.SIGINT, syscall.SIGTERM, syscall.SIGHUP, syscall.SIGQUIT)
-	ctx, stop := signal.NotifyContext(
-		context.Background(),
-		syscall.SIGHUP,
-		syscall.SIGINT,
-		syscall.SIGTERM,
-		syscall.SIGQUIT,
-	)
-	defer stop()
+	defer signal.Stop(sigCh)
+
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+	go func() {
+		sig := <-sigCh
+		log.Printf("%s - shutting down gracefully (received signal: %s); press Ctrl+C again to force", srvName, sig)
+		cancel()
+	}()
 
 	select {
 	case <-ctx.Done():
-		sig := <-sigCh
-		log.Printf(
-			"%s - Server Shutting down gracefully (received signal: '%s'), press Ctrl+C again to force\n",
-			srvName,
-			sig,
-		)
-	case err := <-shutdownErr:
+	case err := <-startUpErr:
 		if err != nil {
-			log.Printf("%s - server startup/runtime failure, error: %v\n", srvName, err) // startup/runtime failure
-		} else {
-			log.Printf(
-				"%s - Server Shutting down gracefully (After server stop serving), press Ctrl+C again to force\n",
-				srvName,
-			)
+			log.Printf("%s - server startup/runtime failure, error: %v", srvName, err) // startup/runtime failure
+			return
 		}
+		log.Printf(
+			"%s - Server Shutting down gracefully (After server stop serving), press Ctrl+C again to force",
+			srvName,
+		)
 
 	}
 
 	// The context is used to inform the server it has 10 seconds to finish
 	// the request it is currently handling
-	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	ctx, cancel = context.WithTimeout(context.Background(), 10*time.Second)
 	defer cancel()
 	if err := srv.Shutdown(ctx); err != nil {
 		_ = srv.Close() // If shutdown times out, force close:
-		log.Printf("%s - Server forced to shutdown: %v\n", srvName, err)
+		log.Printf("%s - Server forced to shutdown: %v", srvName, err)
 		return
 	}
 
-	log.Printf("%s - Server Shutdown, cleaning up resources\n", srvName)
+	log.Printf("%s - Server Shutdown, cleaning up resources", srvName)
 	// TODO: cleanup resources
-	log.Printf("%s - Server exiting\n", srvName)
+	log.Printf("%s - Server exiting", srvName)
 }
 
 func initEnv() *Config {
 	err := godotenv.Load(".env")
 	if err != nil {
-		panic(fmt.Sprintf("Error loading .env file: %s", err.Error()))
+		log.Fatalf("Error loading .env file: %s", err.Error())
 	}
 
 	config := NewConfig(DefaultConfigName)
